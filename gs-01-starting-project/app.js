9a629eb131a77ae847533116ff96b44b35f9e2f1
Eun <euncheol.jang@howoocast.com>
2022-12-12 17:00:05 +0900

    Vue로 앱 제구축하기

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,13 +1,28 @@
-const buttonEl = document.querySelector("button");
-const inputEl = document.querySelector("input");
-const listEl = document.querySelector("ul");
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
+    },
+  },
+}).mount("#app");
 
-function addGoal() {
-  const enterdValue = inputEl.value;
-  const listItemEl = document.createElement("li");
-  listItemEl.textContent = enterdValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = "";
-}
+// const buttonEl = document.querySelector("button");
+// const inputEl = document.querySelector("input");
+// const listEl = document.querySelector("ul");
 
-buttonEl.addEventListener("click", addGoal);
+// function addGoal() {
+//   const enterdValue = inputEl.value;
+//   const listItemEl = document.createElement("li");
+//   listItemEl.textContent = enterdValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = "";
+// }
+
+// buttonEl.addEventListener("click", addGoal); 
