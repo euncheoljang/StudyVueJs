2db387e06543b773bf2dda234d8423b53e62060c
Eun <euncheol.jang@howoocast.com>
2022-12-15 14:55:14 +0900

    과제 1 제출 출

--- basics-assignment-1-problem/app.js
+++ basics-assignment-1-problem/app.js
@@ -1,3 +1,23 @@
-const app = Vue.createApp();
-
+const app = Vue.createApp({
+  data() {
+    return {
+      enterdValue: "",
+      name: this.enterdValue,
+      age: 3,
+      image: "https://cdn.pixabay.com/photo/2022/12/10/11/05/snow-7646952_960_720.jpg",
+    }
+  },
+  methods: {
+    plusFive(age) {
+      return age + 5;
+    },
+    favoritNum() {
+      return Math.random();
+    },
+    addName(enterdValue) {
+      this.enterdValue += enterdValue.data
+      this.name = this.enterdValue
+    }
+  }
+});
 app.mount("#assignment")
