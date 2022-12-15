caf8f85e690d3b5e535f8bf8d0cc2453a547f4e9
Eun <euncheol.jang@howoocast.com>
2022-12-15 16:00:21 +0900

    네이티브 이벤트 객체 사용하기

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,15 +2,19 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
   },
   methods: {
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
     add(number) {
       this.counter = this.counter + number;
     },
     reduce(number) {
       this.counter = this.counter - number;
-    }
+    },
   }
 });
 
